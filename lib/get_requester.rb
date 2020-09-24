5a96c04dd1ab5ff41310fd2284dd9826b2d5dd22
Dexter Fitch <fitch.dexter@gmail.com>
2020-09-24 22:39:44 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'json'
 require 'pry'
 
-class GetRequester(inputURL)
+class GetRequester
 
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
